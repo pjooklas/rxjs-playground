b2c4a22290c3e3b0ab18d4dd9b5735c24032d89b
Laurynas <pjooklas@gmail.com>
2022-12-19 08:39:53 +0200

    subscriber error handle

--- src/index.js
+++ src/index.js
@@ -5,10 +5,8 @@ var observable$ = new rxjs_1.Observable(function (sub) {
     console.log('Observable executed');
     sub.next('Alice');
     sub.next('Ben');
-    setTimeout(function () {
-        sub.next('Charlie'),
-            sub.complete();
-    }, 2000);
+    setTimeout(function () { return sub.next('Charlie'); }, 2000);
+    setTimeout(function () { return sub.error(new Error('Failure')); }, 4000);
     return function () {
         console.log('Teardown');
     };
@@ -16,6 +14,7 @@ var observable$ = new rxjs_1.Observable(function (sub) {
 console.log('Before subscribe');
 observable$.subscribe({
     next: function (value) { return console.log(value); },
+    error: function (err) { return console.log(err.message); },
     complete: function () { return console.log('complete'); }
 });
 console.log('After subscribe');
--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,8 @@ const observable$ = new Observable<string>(sub => {
   console.log('Observable executed');
   sub.next('Alice');
   sub.next('Ben');
-  setTimeout(() => {
-      sub.next('Charlie'),
-     sub.complete()
-  }, 2000);
+  setTimeout(() => sub.next('Charlie'), 2000);
+  setTimeout(() => sub.error(new Error('Failure')), 4000);
 
   return () => {
     console.log('Teardown');
@@ -17,6 +15,7 @@ const observable$ = new Observable<string>(sub => {
 console.log('Before subscribe');
 observable$.subscribe({
   next: value => console.log(value),
+  error: err => console.log(err.message),
   complete: () => console.log('complete'),
 });
 console.log('After subscribe');
